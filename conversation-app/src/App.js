f42b304149aa92932a40dff87acc8d6dd558d119
Rob Spain <rob.spain@example.com>
2025-03-20 11:15:40 -0700

    Add conversation-app as regular directory instead of submodule

---
+++ conversation-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>K-1 SDC Assessment</h1>
+        <p>
+          A responsive web application for managing conversations
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
